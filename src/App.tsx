79e6839d9db11ebb4e8ffeca34de08e5481089d4
Yashita <yashita151293@gmail.com>
2024-06-04 13:42:08 +0530

    Fixed routes-3

--- src/App.tsx
+++ src/App.tsx
@@ -18,14 +18,14 @@ const Content: React.FC = () => {
   return (
     <div className='h-screen flex flex-col justify-center'>
       <h1 className="p-5 text-3xl text-center">Data Table Samples</h1>  
-      {location.pathname === "https://yashita1512.github.io/ReusableTableComponent/" && <p className="text-xl text-center">Click on one of the examples to see the corressponding tables</p>} 
-      <nav className="flex justify-center gap-4 pt-8">
+      <nav className="flex justify-center gap-4 p-8">
         <Link to="/ReusableTableComponent">Home |</Link>
         <Link to="/ReusableTableComponent/example1">Example 1 |</Link>
         <Link to="/ReusableTableComponent/example2">Example 2 |</Link>
         <Link to="/ReusableTableComponent/example3">Example 3</Link>       
       </nav>
       <Routes>
+        <Route path="/ReusableTableComponent" element={<p className="text-xl text-center">Click on one of the examples to see the corressponding tables</p>}/>
         <Route path="/ReusableTableComponent/example1" element={<Page heading={"NPATG-Game"} heading_arr={NPATG_headings} data_arr={NPATG_data} />} />
         <Route path="/ReusableTableComponent/example2" element={<Page heading={"Customer-Data"} heading_arr={Customer_headings} data_arr={Customer_data} />} />
         <Route path="/ReusableTableComponent/example3" element={<Page heading={"Product-Data"} heading_arr={Product_headings} data_arr={Product_data} />} />
